724cc0e8f768600bdc5fedb2612258ae063f6ae3
Nachtje <datanerd07@gmail.com>
2024-02-29 13:04:06 +0100

    cleanup records with empty prices

--- clean.py
+++ clean.py
@@ -17,9 +17,38 @@ def removedup_id(df):
     dup = df.duplicated(subset=["property_id"]).sum()
     print("Number of duplicates AFTER:",dup)
 
-print("Removing Duplicates from Houses")
+def remove_none_prices(df):
+    price_empty_before = df["price"].isnull().sum()
+    print("Number of records with empty price BEFORE:", price_empty_before)
+    df.dropna(subset=['price'], inplace=True)
+    price_empty_after = df["price"].isnull().sum()
+    print("Number of records with empty price AFTER:", price_empty_after)
+    return df
+
+
+
+
+    
+print("-------------------------------")
+print("TOTAL HOUSE RECORDS:",len(house))
+print("TOTAL APP RECORDS:",len(app))
+print("-------------------------------")
+print("---Removing Duplicates from Houses")
 removedup_id(house)
-print("Removing Duplicates from Appartments")
+print("---Removing Duplicates from Appartments")
 removedup_id(app)
+print("-------------------------------")
+print("TOTAL HOUSE RECORDS:",len(house))
+print("TOTAL APP RECORDS:",len(app))
+print("-------------------------------")
+print("---Removing records with empty price field from Houses")
+remove_none_prices(house)
+print("---Removing records with empty price field from Appartements")
+remove_none_prices(app)
+print("-------------------------------")
+print("TOTAL HOUSE RECORDS:",len(house))
+print("TOTAL APP RECORDS:",len(app))
+print("-------------------------------")
+
 
 
